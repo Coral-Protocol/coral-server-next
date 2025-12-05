110e3a3d77340ba8e1a27cef7c97c7fa67b43346
Séafra <Forder>
2025-12-05 19:02:02 +0100

    partial implementation of instruction snippets

---
+++ src/main/kotlin/org/coralprotocol/coralserver/mcp/McpInstructionSnippet.kt
@@ -0,0 +1,59 @@
+package org.coralprotocol.coralserver.mcp
+
+enum class McpInstructionSnippet(
+    val snippet: String
+) {
+    MESSAGING("""
+        # Messaging
+        
+        Agent to agent communication is important for a Coral agent and in Coral this is done with messages.  Messages 
+        can be sent using the ${McpToolName.SEND_MESSAGE} tool.  All messages must be sent into Coral threads.
+        
+        # Threads
+        
+        In Coral, messaging is done exclusively in threads.  Coral threads are created by agents who have access to the 
+        ${McpToolName.CREATE_THREAD} tool.  Threads should be created when a new subject of conversation is started, 
+        and should be closed when the conversation reaches a summary, using the ${McpToolName.CLOSE_THREAD} tool.  
+       
+        You cannot send messages into a closed thread.
+        
+        Threads have participants, agents who are not participating in a thread will not see the messages in the thread.
+        Participants are set during the creation of a thread but can also be added or removed later using the 
+        ${McpToolName.ADD_PARTICIPANT} and ${McpToolName.REMOVE_PARTICIPANT} tools respectively.  If the discussion in
+        a thread evolves and could benefit from a new participant ${McpToolName.ADD_PARTICIPANT} should be used as soon
+        as possible.  Vice versa, if a participant is no needed in a thread ${McpToolName.REMOVE_PARTICIPANT} should be 
+        used as soon as possible.
+    """.trimIndent()),
+
+    MENTIONS("""
+        # Mentions
+        
+        When posting a message to a thread, you can optionally specify agents that should be "mentioned" in the message.
+        There is a higher chance that the agent will read, and consider important, a message that mentions them.  If 
+        your message intends to directly seek input from or respond to an agent, you should mention them.
+    """.trimIndent()),
+
+    WAITING("""
+        # Waiting
+       
+        To complete your tasks to your best ability, you often need more information than you currently have.  In Coral,
+        each agent is able to act asynchronously so it is important that when you need more input to perform your task 
+        effectively that you call one of the available waiting tools.
+        
+        ## Tool 1: ${McpToolName.WAIT_FOR_MESSAGE}
+        
+        The ${McpToolName.WAIT_FOR_MESSAGE} tool will wait until one message, from any agent, is received.  This tool 
+        should only be used if it is not clear what agent has information you would benefit from.
+        
+        ## Tool 2: ${McpToolName.WAIT_FOR_AGENT}
+        
+        The ${McpToolName.WAIT_FOR_AGENT} tool will wait for one message posted by a specific agent in any thread. 
+        This tool is useful if you know that a specific agent has information you would benefit from.
+        
+        ## Tool 3: ${McpToolName.WAIT_FOR_MENTION}
+        
+        The ${McpToolName.WAIT_FOR_MENTION} tool will wait until any message that mentions you is received.  This is the
+        best tool to use if another agent is likely to know how best to utilise your information.  If there exists any
+        agent that plays a managerial role in the conversation, this tool should be used.
+    """.trimIndent());
+}
