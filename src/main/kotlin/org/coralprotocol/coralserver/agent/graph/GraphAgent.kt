673d6c7f05e8ee225a0a08c2ec1b21bb3e5bd735
Séafra <Forder>
2025-12-13 18:47:51 +0100

    reg3 refactor:
    renaming in GraphAgent

--- src/main/kotlin/org/coralprotocol/coralserver/agent/graph/GraphAgent.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/agent/graph/GraphAgent.kt
@@ -1,13 +1,15 @@
 package org.coralprotocol.coralserver.agent.graph
 
 import org.coralprotocol.coralserver.agent.graph.plugin.GraphAgentPlugin
-import org.coralprotocol.coralserver.agent.registry.*
+import org.coralprotocol.coralserver.agent.registry.AgentExportSettings
+import org.coralprotocol.coralserver.agent.registry.AgentRegistry
+import org.coralprotocol.coralserver.agent.registry.RegistryAgent
+import org.coralprotocol.coralserver.agent.registry.RegistryAgentIdentifier
 import org.coralprotocol.coralserver.agent.registry.option.AgentOptionWithValue
 import org.coralprotocol.coralserver.routes.api.v1.Sessions
 import org.coralprotocol.coralserver.session.LocalSession
 import org.coralprotocol.coralserver.session.remote.RemoteSession
 import org.coralprotocol.coralserver.x402.X402BudgetedResource
-import java.util.*
 
 /**
  * Coral agent modeling
@@ -18,7 +20,7 @@ import java.util.*
  * reference to the [RegistryAgent] that is to be exported and pricing information.  It is an invalid configuration to
  * export an agent that is not itself imported.
  *
- * Every agent in the registry is identified using [AgentRegistryIdentifier].  A registry is guaranteed to only have one agent
+ * Every agent in the registry is identified using [RegistryAgentIdentifier].  A registry is guaranteed to only have one agent
  * with a given identifier, it is an invalid configuration to have more than one agent with the same identifier.
  *
  * The use of agents in Coral server happens exclusively within sessions, either a [LocalSession] or a [RemoteSession].
