99a715a57cd40d758d2ef3bf493d589dc5a0ccb7
Séafra <Forder>
2025-12-16 14:33:37 +0100

    fixed ServerEvent (for real)

--- src/main/kotlin/org/coralprotocol/coralserver/events/ServerEvent.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/events/ServerEvent.kt
@@ -8,25 +8,22 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonClassDiscriminator
 import org.coralprotocol.coralserver.session.SessionId
 
-/**
- * TODO: where should these come from?
- */
 @Serializable
 @JsonClassDiscriminator("type")
-class ServerEvent {
+sealed interface ServerEvent {
     @Serializable
     @SerialName("session_created")
-    data class SessionCreated(val sessionId: SessionId, val namespace: String) : SessionEvent
+    data class SessionCreated(val sessionId: SessionId, val namespace: String) : ServerEvent
 
     @Serializable
     @SerialName("session_closed")
-    data class SessionClosed(val sessionId: SessionId, val namespace: String) : SessionEvent
+    data class SessionClosed(val sessionId: SessionId, val namespace: String) : ServerEvent
 
     @Serializable
     @SerialName("namespace_created")
-    data class NamespaceCreated(val namespace: String) : SessionEvent
+    data class NamespaceCreated(val namespace: String) : ServerEvent
 
     @Serializable
     @SerialName("namespace_closed")
-    data class NamespaceClosed(val namespace: String) : SessionEvent
+    data class NamespaceClosed(val namespace: String) : ServerEvent
 }
