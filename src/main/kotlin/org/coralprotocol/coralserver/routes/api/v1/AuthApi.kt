9991b5e504b279f7ecb963473de5d0450953e755
Séafra <Forder>
2025-12-17 20:23:06 +0100

    sync auth API with console

--- src/main/kotlin/org/coralprotocol/coralserver/routes/api/v1/AuthApi.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/routes/api/v1/AuthApi.kt
@@ -3,6 +3,7 @@ package org.coralprotocol.coralserver.routes.api.v1
 import io.github.smiley4.ktoropenapi.resources.post
 import io.ktor.http.*
 import io.ktor.resources.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
@@ -14,18 +15,19 @@ import org.coralprotocol.coralserver.server.RouteException
 @Resource("auth")
 class Auth(val parent: ApiV1 = ApiV1()) {
 
-    @Resource("token/{token}")
-    class Token(val parent: Auth = Auth(), val token: String)
+    @Resource("token")
+    class Token(val parent: Auth = Auth())
 }
 
 fun Route.authApi(config: Config) {
     post<Auth.Token>({
         hidden = true
     }) { path ->
-        if (!config.auth.keys.contains(path.token))
+        val token = call.receiveParameters()["token"]
+        if (token == null || !config.auth.keys.contains(token))
             throw RouteException(HttpStatusCode.Unauthorized, "Invalid token")
 
-        call.sessions.set(AuthSession.Token(path.token))
-        call.respondRedirect(call.parameters["redirect_to"] ?: "/ui/console/")
+        call.sessions.set(AuthSession.Token(token))
+        call.respondRedirect(call.parameters["to"] ?: "/ui/console/")
     }
 }
