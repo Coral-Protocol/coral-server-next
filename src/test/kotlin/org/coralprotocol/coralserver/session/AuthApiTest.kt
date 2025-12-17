2e561cc5fcfb1318c562df89fb780bbbcfd2614b
Séafra <Forder>
2025-12-17 20:27:49 +0100

    update session auth tests for new form input type

--- src/test/kotlin/org/coralprotocol/coralserver/session/AuthApiTest.kt
+++ src/test/kotlin/org/coralprotocol/coralserver/session/AuthApiTest.kt
@@ -3,6 +3,7 @@ package org.coralprotocol.coralserver.session
 import io.kotest.assertions.ktor.client.shouldHaveStatus
 import io.kotest.core.spec.style.FunSpec
 import io.ktor.client.plugins.resources.*
+import io.ktor.client.request.forms.*
 import io.ktor.http.*
 import org.coralprotocol.coralserver.routes.api.v1.Auth
 import org.coralprotocol.coralserver.routes.api.v1.Registry
@@ -12,16 +13,28 @@ class AuthApiTest : FunSpec({
     test("testAuthSession") {
         sessionTest {
             ktor.client.get(Registry()).shouldHaveStatus(HttpStatusCode.Unauthorized)
-            ktor.client.post(Auth.Token(token = authToken)).shouldHaveStatus(HttpStatusCode.Found)
+            ktor.client.submitForm(
+                url = ktor.client.href(Auth.Token()),
+                formParameters = parameters {
+                    append("token", authToken)
+                }
+            ).shouldHaveStatus(HttpStatusCode.Found)
             ktor.client.get(Registry()).shouldHaveStatus(HttpStatusCode.OK)
         }
     }
 
     test("testAuthWebSocket") {
         sessionTest {
-            ktor.client.get(Events.SessionEvents(namespace = "test", sessionId = "test")).shouldHaveStatus(HttpStatusCode.Unauthorized)
-            ktor.client.post(Auth.Token(token = authToken)).shouldHaveStatus(HttpStatusCode.Found)
-            ktor.client.get(Events.SessionEvents(namespace = "test", sessionId = "test")).shouldHaveStatus(HttpStatusCode.NotFound)
+            ktor.client.get(Events.SessionEvents(namespace = "test", sessionId = "test"))
+                .shouldHaveStatus(HttpStatusCode.Unauthorized)
+            ktor.client.submitForm(
+                url = ktor.client.href(Auth.Token()),
+                formParameters = parameters {
+                    append("token", authToken)
+                }
+            ).shouldHaveStatus(HttpStatusCode.Found)
+            ktor.client.get(Events.SessionEvents(namespace = "test", sessionId = "test"))
+                .shouldHaveStatus(HttpStatusCode.NotFound)
         }
     }
 })
