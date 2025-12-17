38ce12e8b5dce64329b686ce86290066344d95b7
Séafra <Forder>
2025-12-17 19:24:46 +0100

    added test for ScopedFlow

--- src/test/kotlin/org/coralprotocol/coralserver/session/ScopedFlowTest.kt
+++ src/test/kotlin/org/coralprotocol/coralserver/session/ScopedFlowTest.kt
@@ -2,19 +2,21 @@ package org.coralprotocol.coralserver.session
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import org.coralprotocol.coralserver.util.EventFlow
+import org.coralprotocol.coralserver.util.ScopedFlow
 import kotlin.time.Duration.Companion.seconds
 
-class EventFlowTest : FunSpec({
+class ScopedFlowTest : FunSpec({
     test("testMultipleListeners").config(timeout = 5.seconds) {
         val maxListeners = 10
         val maxMessages = 100
 
-        val eventFlow = EventFlow<Int>(this)
+        val scopedFlow = ScopedFlow<Int>()
         val listenerCount = MutableStateFlow(0)
         val totalCollected = MutableStateFlow(0)
 
@@ -23,7 +25,7 @@ class EventFlowTest : FunSpec({
                 listenerCount.update { it + 1 }
 
                 var collectCount = 0
-                eventFlow.collectUntilClosed { _ ->
+                scopedFlow.collectUntilCanceled { _ ->
                     collectCount++
                     totalCollected.update { it + 1 }
                 }
@@ -34,12 +36,41 @@ class EventFlowTest : FunSpec({
         listenerCount.first { it == maxListeners }
 
         repeat(maxMessages) {
-            eventFlow.emit(1)
+            scopedFlow.emit(1)
         }
 
         totalCollected.first {
             it == maxListeners * maxMessages
         }
-        eventFlow.close()
+        scopedFlow.close()
+    }
+
+    test("testFailingListener") {
+        val scopedFlow = ScopedFlow<Int>()
+        val collectorCount = MutableStateFlow(0)
+        val messageCount = 10
+        launch {
+            var count = 0
+            collectorCount.update { it + 1 }
+            scopedFlow.collectUntilCanceled {
+                count++
+            }
+
+            count.shouldBe(messageCount)
+        }
+
+        launch {
+            collectorCount.update { it + 1 }
+            scopedFlow.collectUntilCanceled {
+                cancel()
+            }
+        }
+
+        collectorCount.first { it == 2 }
+        repeat(messageCount) {
+            scopedFlow.emit(1)
+        }
+
+        scopedFlow.close()
     }
 })
