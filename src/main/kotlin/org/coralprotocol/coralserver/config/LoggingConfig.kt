1ba2fa7385d4a041d32f75117065be4539ca7c45
Séafra <Forder>
2026-01-04 15:51:05 +0100

    added configuration for file based logs to LoggingConfig

--- src/main/kotlin/org/coralprotocol/coralserver/config/LoggingConfig.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/config/LoggingConfig.kt
@@ -1,5 +1,7 @@
 package org.coralprotocol.coralserver.config
 
+import java.nio.file.Path
+
 data class LoggingConfig(
     /**
      * The number of logging events to store in memory
@@ -9,5 +11,31 @@ data class LoggingConfig(
     /**
      * Maximum number of logging events to replay to a new subscriber, regardless of what they requested
      */
-    val maxReplay: UInt = 2048u
+    val maxReplay: UInt = 2048u,
+
+    /**
+     * https://logback.qos.ch/manual/appenders.html#tbrpFileNamePattern
+     */
+    val logFileNamePattern: String = Path.of(System.getProperty("user.home"), ".coral", "logs")
+        .toString() + "/%d{yyyy/MM, aux}/%d{yyyy-MM-dd}.%i.log.gz",
+
+    /**
+     * https://logback.qos.ch/manual/appenders.html#tbrpMaxHistory
+     */
+    val maxHistory: Int = 12,
+
+    /**
+     * https://logback.qos.ch/manual/appenders.html#totalSizeCap
+     */
+    val logTotalSizeCap: String = "3GB",
+
+    /**
+     * https://logback.qos.ch/manual/appenders.html#tbrpCleanHistoryOnStart
+     */
+    val logClearHistoryOnStart: Boolean = false,
+
+    /**
+     * https://logback.qos.ch/manual/appenders.html#maxFileSize
+     */
+    val maxFileSize: String = "10MB"
 )
--- src/main/kotlin/org/coralprotocol/coralserver/modules/LoggingModule.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/modules/LoggingModule.kt
@@ -2,12 +2,19 @@ package org.coralprotocol.coralserver.modules
 
 import org.coralprotocol.coralserver.config.LoggingConfig
 import org.coralprotocol.coralserver.logging.Logger
-import org.coralprotocol.coralserver.logging.LoggingInterface
 import org.koin.dsl.module
 
 val loggingModule = module {
-    single {
+    single(createdAtStart = true) {
+
         val config by inject<LoggingConfig>()
+
+        System.setProperty("FILE_NAME_PATTERN", config.logFileNamePattern)
+        System.setProperty("MAX_HISTORY", config.maxHistory.toString())
+        System.setProperty("TOTAL_SIZE_CAP", config.logTotalSizeCap)
+        System.setProperty("CLEAR_HISTORY_ON_START", config.logClearHistoryOnStart.toString())
+        System.setProperty("MAX_FILE_SIZE", config.maxFileSize)
+
         Logger(config.logBufferSize.toInt())
     }
 }
